b0ca9619183cf0e06b184b47e23ec5772d3b1f4d
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-17 17:43:34 +0500

    ok

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -58,15 +58,15 @@ export default function AdminCategoryPage() {
 
       {/* Main Content */}
       <main className="flex-1 p-6">
-        {/* <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-semibold">Category Products</h1>
-          <button
+          {/* <button
             className="px-4 py-2 bg-green-600 text-white rounded"
             onClick={() => setIsAddModalOpen(true)}
           >
             Add Product
-          </button>
-        </div> */}
+          </button> */}
+        </div>
 
         {/* Category Buttons */}
         <div className="flex gap-2 mb-6 flex-wrap">
